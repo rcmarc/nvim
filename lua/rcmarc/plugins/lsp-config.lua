582c91bbbc93603ade361e52d707d74687447fb5
Marcos Carrera <marcos.crs@yahoo.com>
2026-05-18 16:41:41 -0400

    update code action shortcut cause it conflicts with getting char info

--- lua/rcmarc/plugins/lsp-config.lua
+++ lua/rcmarc/plugins/lsp-config.lua
@@ -21,7 +21,7 @@ return {
 					vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 					vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
 					vim.keymap.set("n", "gf", function() vim.diagnostic.open_float() end, opts)
-					vim.keymap.set("n", "ga", function() vim.lsp.buf.code_action() end, opts)
+					vim.keymap.set("n", "gca", function() vim.lsp.buf.code_action() end, opts)
 					vim.keymap.set("n", "gr", function() vim.lsp.buf.rename() end, opts)
 					vim.keymap.set("n", "gt", function() vim.lsp.buf.references() end, opts)
 					vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
