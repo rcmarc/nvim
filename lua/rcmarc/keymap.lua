175131185a3ff05603297e5b09260c561eae2dda
Marcos Carrera <marcos.crs@yahoo.com>
2026-07-14 12:53:47 -0400

    added shortcut to toggle word wrap

--- lua/rcmarc/keymap.lua
+++ lua/rcmarc/keymap.lua
@@ -5,3 +5,7 @@ vim.keymap.set({'i', 'n', 'v'}, '<C-C>', '<esc>', { desc = 'Make Ctrl+C behave e
 -- clipboard
 vim.keymap.set({'n', 'v'}, '<leader>y','"+y')
 vim.keymap.set({'n', 'v'}, '<leader>p','"+p')
+
+-- toggle word wrap
+vim.keymap.set('n', '<leader>tw', ':set wrap!<CR>', { silent = true, desc = 'Toggle Wrap' })
+
