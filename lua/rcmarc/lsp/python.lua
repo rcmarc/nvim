23e0cca0fe3a0a8175c128dbce09286d6a570fdf
Marcos Carrera <marcos.crs@yahoo.com>
2026-08-06 15:09:09 -0400

    updated python lsp

--- lua/rcmarc/lsp/python.lua
+++ lua/rcmarc/lsp/python.lua
@@ -1,1 +1,18 @@
-vim.lsp.enable('basedpyright')
+vim.lsp.config('zubanls', {
+	name = "ZubanLS",
+	cmd = { "zuban", "server" },
+	root_markers = { "pyproject.toml", ".git" },
+	filetypes = { "python" },
+})
+
+vim.lsp.enable("zubanls")
+
+vim.lsp.config('ruff', {
+	init_options = {
+		settings = {
+			-- Ruff language server settings go here
+		}
+	}
+})
+
+vim.lsp.enable('ruff')
