a62f55427d00fa92132742885d1debe55b2ffb15
Marcos Carrera <marcos.crs@yahoo.com>
2026-08-14 08:36:07 -0400

    use leader as prefix for lsp keymaps

--- lua/rcmarc/plugins/lsp-config.lua
+++ lua/rcmarc/plugins/lsp-config.lua
@@ -12,22 +12,22 @@ return {
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
 					if client and client:supports_method('textDocument/completion') then
 						-- Enable completion and set autotrigger to true for automatic popup
-						vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true } )
+						vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 						vim.keymap.set('i', '<c-space>', function()
 							vim.lsp.completion.get()
 						end)
 					end
 					local opts = { buffer = args.buf }
-					vim.keymap.set("n", "gq", function() vim.lsp.buf.format() end, opts)
-					vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-					vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
-					vim.keymap.set("n", "gf", function() vim.diagnostic.open_float() end, opts)
-					vim.keymap.set("n", "gca", function() vim.lsp.buf.code_action() end, opts)
-					vim.keymap.set("n", "gr", function() vim.lsp.buf.rename() end, opts)
-					vim.keymap.set("n", "gt", function() vim.lsp.buf.references() end, opts)
+					vim.keymap.set("n", "<leader>gq", function() vim.lsp.buf.format() end, opts)
+					vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+					vim.keymap.set("n", "<leader>gh", function() vim.lsp.buf.hover() end, opts)
+					vim.keymap.set("n", "<leader>gf", function() vim.diagnostic.open_float() end, opts)
+					vim.keymap.set("n", "<leader>gca", function() vim.lsp.buf.code_action() end, opts)
+					vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.rename() end, opts)
+					vim.keymap.set("n", "<leader>gt", function() vim.lsp.buf.references() end, opts)
 					vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-					vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
-					vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+					vim.keymap.set("n", "<leader>]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
+					vim.keymap.set("n", "<leader>[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
 				end
 			})
 		end
